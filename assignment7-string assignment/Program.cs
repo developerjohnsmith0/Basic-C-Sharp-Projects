cd060006fcba281422af940d7df500c7855dde1a
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-09 11:36:04 -0500

    string assignment

---
+++ assignment7-string assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+namespace String_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Robert ";
+            string middleName = "Downey ";
+            string lastName = "Junior ";
+            StringBuilder fullName = new StringBuilder();
+            fullName.Append(string.Concat(firstName.ToUpper(), middleName.ToUpper(), lastName.ToUpper()));
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+    }
+}
