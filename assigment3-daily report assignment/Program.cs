67689b34dd00ea29f913d2743bf06d9b3f972c86
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-11 10:51:30 -0500

    daily report assignment

---
+++ assigment3-daily report assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College.");
+            Console.WriteLine("Student Daily Report:");
+            Console.WriteLine("What course are you in?");
+            string appliedCourseOfStudent = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool isAnyHelpNeeded = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you would like to share? Please give specifics.");
+            string ecperienceOfStudent = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you would like to provide? Please be specific.");
+            string feedbackOfStudent = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int studyHoursOfStudent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+        }
+    }
+}
