274ed3df7164d6d3cf58bb9c26dd4eeba27d89e1
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-11 10:49:55 -0500

    boolean logic assignment

---
+++ assignment2-boolean logic assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Boolean_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? type [true/false]");
+            string DUI = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have");
+            int userSpeedingTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualify");
+            bool qualification = (userAge >= 15 && DUI == "false" && userSpeedingTickets <= 3);
+            Console.WriteLine(qualification);
+        }
+    }
+}
