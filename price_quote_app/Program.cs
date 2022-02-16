41e936c1ef3ecdb5c22e9d7e1f312df162c8156e
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-16 11:53:31 -0500

    price quote app

---
+++ price_quote_app/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace price_quote_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            double weightOfPackage = Convert.ToDouble(Console.ReadLine());
+            if (weightOfPackage > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                double widthOfPackage = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                double heightOfPackage = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                double lengthOfPackage = Convert.ToDouble(Console.ReadLine());
+                if (widthOfPackage + heightOfPackage + lengthOfPackage > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    double dimensionOfProduct = (widthOfPackage * heightOfPackage * lengthOfPackage);
+                    double productOutcome = (dimensionOfProduct * weightOfPackage);
+                    double priceQuote = (productOutcome / 100);
+
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + priceQuote);
+                    Console.WriteLine("Thank you");
+                }
+            }
+        }
+    }
+}
