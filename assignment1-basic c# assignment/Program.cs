075cc750440df2d162800158b1639afba25c2aa9
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-16 11:52:04 -0500

    basic c# assignment

---
+++ assignment1-basic c# assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Basic_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \n");
+            Console.Read();
+        }
+    }
+}
