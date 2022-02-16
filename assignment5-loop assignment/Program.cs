86f84615363349654c8f0c9a16028808928f00c6
developerjohnsmith0 <87449450+developerjohnsmith0@users.noreply.github.com>
2022-02-16 11:54:37 -0500

    loop assignment

---
+++ assignment5-loop assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace loop_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool guessedNumber = number == 0;
+
+            while (number >= 0)
+            {
+                do
+                {
+                    switch (number)
+                    {
+                        case 10:
+                            Console.WriteLine("You entered 10.Try again");
+                            Console.WriteLine("Guess the number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        case 20:
+                            Console.WriteLine("You entered 20.Try again");
+                            Console.WriteLine("Guess the number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+
+                        default:
+                            Console.WriteLine("You neither entered 10 nor 20");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+                while (!guessedNumber);
+            }
+            Console.Read();
+        }
+    }
+}
